37e3850d3862ac9b9b0269f150a257e74f3923c7
Kathirvel <115524887+KATHIRVEL-13@users.noreply.github.com>
2025-02-02 18:56:11 +0530

    Update backup_a_file.sh

--- backup_a_file.sh
+++ backup_a_file.sh
@@ -13,3 +13,4 @@ cp -r "$SOURCE" "$DESTINATION/$FILE-$DATE"
 
 echo "Backup completed as $FILE-$DATE"
 # it creates backup file this - "kathir.txt-2025-02-02_17-42-12" 
+#if you need to schedule this backup then use crontab -e like this - * * * * * /home/redhat/./backup.sh  note: don't use quotes in crontab -e
